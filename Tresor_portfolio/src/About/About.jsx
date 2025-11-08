ae5f6720e25e5c11f0b1e269780bffa78c94343d
TresorBitangimana <Bitangimanatresor@gmail.com>
2025-11-08 08:47:35 -0600

    added content to about

--- Tresor_portfolio/src/About/About.jsx
+++ Tresor_portfolio/src/About/About.jsx
@@ -4,7 +4,18 @@ import "./About.css";
 function About(){
     return(
         <div className="about-container" id="about-contaienr">
-            <h1>working on it...</h1>
+            <div className="about-content-container" id="about-content-container">
+                <nav className="about-nav" id="about-nav">
+                    <ul>
+                        <li className="about-paths" id="about-paths">Education</li>
+                        <li className="about-paths" id="about-paths">Me</li>
+                        <li className="about-paths" id="about-paths">Interest</li>
+                        <li className="about-paths" id="about-paths">More info</li>
+                    </ul>
+                </nav>
+                <div className="about-info-container">
+                </div>
+            </div>
         </div>
     );
 }
